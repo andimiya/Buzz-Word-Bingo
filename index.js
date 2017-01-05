c0490ca8a3db8da6e6c3b8884a5375c78c53c39b
Andrea <takamiya.andrea@gmail.com>
2017-01-04 21:16:10 -1000

    GET / Serves the static index.html file

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3000;
 
+app.use('/', express.static('public'));
 
 
 var server = app.listen(PORT, () => {
