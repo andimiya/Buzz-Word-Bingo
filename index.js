862e3afb38e37f7a44e27e40ba677817599621bb
Andrea <takamiya.andrea@gmail.com>
2017-01-04 20:51:42 -1000

    Initial setup stuff

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT;
+
+
+
+var server = app.listen(PORT, () => {
+  var localhost = server.address().address;
+  var port = server.address().port;
+
+  console.log('PORT:', port);
+});
