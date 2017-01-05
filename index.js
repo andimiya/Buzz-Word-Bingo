2e4c2daa8662a195cf7f310ab60772891774a740
Andrea <takamiya.andrea@gmail.com>
2017-01-04 22:10:53 -1000

    GET /buzzwords response completed

--- index.js
+++ index.js
@@ -3,9 +3,15 @@ const app = express();
 const bodyParser = require('body-parser');
 const PORT = process.env.PORT || 3000;
 
+var buzzwordArray = [];
+
 app.use(bodyParser.urlencoded({extended: false }));
 
 app.get('/', express.static('public'));
+
+app.get('/buzzwords', function (req, res){
+  res.send({"buzzwords": buzzwordArray});
+});
 
 app.post('/buzzword', function (req, res) {
   var buzzWord = req.body.buzzWord;
