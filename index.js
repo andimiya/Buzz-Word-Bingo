74c140d78dddab18422a984f3f5c2d1c85d25218
Andrea <takamiya.andrea@gmail.com>
2017-01-05 18:54:41 -1000

    Delete works, everything working except for some weirdness with logging success:true or success:false

--- index.js
+++ index.js
@@ -20,6 +20,7 @@ app.post('/buzzword', function (req, res) {
   buzzwordArray.push(req.body);
   if (res.send(buzzwordArray) ){
     console.log({"success": true});
+    return;
   }
   else {
     console.log({"success": false});
@@ -33,9 +34,22 @@ app.put('/buzzword', function (req, res) {
       buzzwordArray[i].heard = true;
       newScore = Number(newScore) + Number(buzzwordArray[i].points);
       console.log({"success": true, "newScore": newScore});
+      return;
+    }
+    else {
+      console.log({"success": false});
     }
   }
   res.send(buzzwordArray);
+});
+
+app.delete('/buzzword', function (req, res) {
+  for (var i = 0; i < buzzwordArray.length; i++){
+    if(req.body.buzzWord === buzzwordArray[i].buzzWord){
+      buzzwordArray.splice(i, 1);
+    }
+  }
+  console.log({"success": true});
 });
 
 var server = app.listen(PORT, () => {
